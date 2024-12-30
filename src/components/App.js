cdd1913e8584ae46d8e45a77e1e5b2ef5a4691f3
Stakshi <semwaleishta6@gmail.com>
2024-12-30 18:01:39 +0530

    dsD

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,19 @@
-import { Outlet, Link } from "react-router-dom";
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ItemList from './ItemList'
+import ItemDetail from './ItemDetail'
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <ul>
-     <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Item1">Item1</Link>
-          </li>
-          <li>
-            <Link to="/Item2">Item2</Link>
-          </li>
-          <li>
-            <Link to="/Item3">Item3</Link>
-          </li>
-        </ul>
-     <Outlet/>
+          <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<ItemList />}></Route>
+                <Route path='/items/:id' element={<ItemDetail />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
